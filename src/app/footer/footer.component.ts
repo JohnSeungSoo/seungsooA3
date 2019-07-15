6c1faa3c20b6631373048292fad841ae4fc864b8
JohnSeungsoo <seungsoo@sheridancollege.ca>
2019-07-15 19:09:52 -0400

    Implement header, footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { formatDate } from '@angular/common';
+import { Seungsoo } from '../seungsoo';
 
 @Component({
   selector: 'app-footer',
@@ -10,12 +11,20 @@ import { formatDate } from '@angular/common';
 export class FooterComponent implements OnInit {
 
 	today: string ="";
+	currSeungsoo : Seungsoo;
 
   constructor() {
 	this.today = formatDate(new Date(), 'yyyy/MM/dd', 'en');
   }
 
   ngOnInit() {
+		this.currSeungsoo = {
+			studentId: 991494887,
+			studentName: "Seungsoo Kang",
+			loginName: "seungsoo",
+			campus: "Trafalgar",
+			assignmentTitle: "Assignment3"
+		};
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Seungsoo } from '../seungsoo';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+	currSeungsoo : Seungsoo;
+
   constructor() { }
 
   ngOnInit() {
-  }
-
+		  this.currSeungsoo = {
+			studentId: 991494887,
+			studentName: "Seungsoo Kang",
+			loginName: "seungsoo",
+			campus: "Trafalgar",
+			assignmentTitle: "Assignment3"
+		};
+	}
+	
 }
