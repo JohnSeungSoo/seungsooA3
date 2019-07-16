568e634147d831226b7682c44a2640414caf7da2
JohnSeungsoo <seungsoo@sheridancollege.ca>
2019-07-16 14:24:05 -0400

    update image location for deployment

--- src/app/myBooks.ts
+++ src/app/myBooks.ts
@@ -11,7 +11,8 @@ export class MyBooks {
 			author: "Bob",
 			genre: "Science",
 			year: "2015",
-			picture: "../assets/images/network.png"
+			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/network.png"
+			// picture: "/assets/images/network.png"
 		};
 
 		this.list[1] = {
@@ -19,7 +20,8 @@ export class MyBooks {
 			author: "Uncle",
 			genre: "Programming",
 			year: "2007",
-			picture: "../assets/images/clean.png"
+			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/clean.png"
+			// picture: "/assets/images/clean.png"
 		};
 
 		this.list[2] = {
@@ -27,7 +29,8 @@ export class MyBooks {
 			author: "Ronak",
 			genre: "Programming",
 			year: "2019",
-			picture: "../assets/images/angular.png"
+			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/angular.png"
+			// picture: "/assets/images/angular.png"
 		};
 
 		this.list[3] = {
@@ -35,7 +38,8 @@ export class MyBooks {
 			author: "John",
 			genre: "Database",
 			year: "2018",
-			picture: "../assets/images/oracle.png"
+			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/oracle.png"
+			// picture: "/assets/images/oracle.png"
 		};
 
 	}
