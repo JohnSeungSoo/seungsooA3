c8a963baf53356c92634ce92fca977dbbedf91aa
JohnSeungsoo <seungsoo@sheridancollege.ca>
2019-07-28 13:41:21 -0400

    Produce Deployment

--- src/app/myBooks.ts
+++ src/app/myBooks.ts
@@ -11,8 +11,8 @@ export class MyBooks {
 			author: "Bob",
 			genre: "Science",
 			year: "2015",
-			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/network.png"
-			// picture: "/assets/images/network.png"
+			// picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/network.png"
+			picture: "/assets/images/network.png"
 		};
 
 		this.list[1] = {
@@ -20,8 +20,8 @@ export class MyBooks {
 			author: "Uncle",
 			genre: "Programming",
 			year: "2007",
-			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/clean.png"
-			// picture: "/assets/images/clean.png"
+			// picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/clean.png"
+			picture: "/assets/images/clean.png"
 		};
 
 		this.list[2] = {
@@ -29,8 +29,8 @@ export class MyBooks {
 			author: "Ronak",
 			genre: "Programming",
 			year: "2019",
-			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/angular.png"
-			// picture: "/assets/images/angular.png"
+			// picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/angular.png"
+			picture: "/assets/images/angular.png"
 		};
 
 		this.list[3] = {
@@ -38,8 +38,8 @@ export class MyBooks {
 			author: "John",
 			genre: "Database",
 			year: "2018",
-			picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/oracle.png"
-			// picture: "/assets/images/oracle.png"
+			// picture: "/SYST24444_Spring_2019/Assignments/Assignment3/assets/images/oracle.png"
+			picture: "/assets/images/oracle.png"
 		};
 
 	}
